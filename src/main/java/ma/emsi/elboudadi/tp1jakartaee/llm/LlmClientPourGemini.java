5556b9c7ea903c34d767e328ea6564761f04e4f2
Lina El Boudadi <Lina.Elboudadi@emsi-edu.ma>
2025-10-30 09:18:52 +0100

    Step 4: Added the LlmClient's code

--- src/main/java/ma/emsi/elboudadi/tp1jakartaee/llm/LlmClientPourGemini.java
+++ src/main/java/ma/emsi/elboudadi/tp1jakartaee/llm/LlmClientPourGemini.java
@@ -1,4 +1,60 @@
 package ma.emsi.elboudadi.tp1jakartaee.llm;
 
-public class LlmClientPourGemini {
+import jakarta.enterprise.context.Dependent;
+import jakarta.ws.rs.client.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.io.Serializable;
+
+/**
+ * Gère l'interface avec l'API de Gemini.
+ * Son rôle est essentiellement de lancer une requête à chaque nouvelle
+ * question qu'on veut envoyer à l'API.
+ *
+ * De portée dependent pour réinitialiser la conversation à chaque fois que
+ * l'instance qui l'utilise est renouvelée.
+ * Par exemple, si l'instance qui l'utilise est de portée View, la conversation est
+ * réunitialisée à chaque fois que l'utilisateur quitte la page en cours.
+ */
+@Dependent
+public class LlmClientPourGemini implements Serializable {
+    // Clé pour l'API du LLM
+    private final String key;
+    // Client REST. Facilite les échanges avec une API REST.
+    private Client clientRest; // Pour pouvoir le fermer
+    // Représente un endpoint de serveur REST
+    private final WebTarget target;
+
+    public LlmClientPourGemini() {
+        // Récupère la clé secrète pour travailler avec l'API du LLM, mise dans une variable d'environnement
+        // du système d'exploitation.
+        // A ECRIRE...
+        this.key = System.getenv("GEMINI_API_KEY"); // OU AUTRE NOM UTILISÉ DANS VOTRE TP
+
+        // Client REST pour envoyer des requêtes vers les endpoints de l'API du LLM
+        this.clientRest = ClientBuilder.newClient();
+
+        // Endpoint REST pour envoyer la question à l'API.
+        // L'URL à trouver a été utilisé dans la commande curl pour tester la clé secrète.
+        // Elle se trouve aussi dans le support de cours.
+        // this.target = clientRest.target("A CHERCHER DANS LE COURS...");
+        this.target = clientRest.target("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent")
+                .queryParam("key", this.key); // Ajout de la clé API comme paramètre de requête
+    }
+
+    /**
+     * Envoie une requête à l'API de Gemini.
+     * @param requestEntity le corps de la requête (en JSON).
+     * @return réponse REST de l'API (corps en JSON).
+     */
+    public Response envoyerRequete(Entity requestEntity) {
+        Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
+        // Envoie la requête POST au LLM
+        return request.post(requestEntity);
+    }
+
+    public void closeClient() {
+        this.clientRest.close();
+    }
 }
